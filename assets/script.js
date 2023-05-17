9e2bce138e5972e0c46fdd9c60c7a07133047aad
ulianim <uliana_usa@yahoo.com>
2023-05-17 15:27:50 -0400

    New changes to the .html and .js

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDay = $("#currentDay")
+
 $(function () {
+  currentDay.text(dayjs())
+  var justTime = dayjs().hour()
+
+    $( ".row" ).each(function( index ) {
+      var rowHour = parseInt($(this).attr("id").split("-")[1])
+      if (justTime === rowHour) {
+        $(this).removeClass( "past future" ).addClass( "present" );
+      } else if (justTime > rowHour) {
+        $(this).removeClass( "present future" ).addClass( "past" );
+      } else {
+        $(this).removeClass( "present past" ).addClass( "future" );
+      }
+    });
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
